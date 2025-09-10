540c6aa01eadd33d221075fbb14356230565ed99
Mauro Nolan <mrnolan88@gmail.com>
2025-09-10 18:53:33 +0000

    primera toma de contacto con fetch

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([])
+	function getTodos(){
+
+	
+	//fetch() -> url del endpoint, enviar el body
+	fetch("https://playground.4geeks.com/todo/users/cenicerolleno",{
+		method:"GET"
+	})
+	// status code , recibe la informacion en JSON, convertimos a JS y l aenviamos 
+	//al siguiente .then()
+	.then((response)=>{
+		console.log(response);
+		return response.json() // .json() convertir de json a JS y enviarlo al siguiente
+	})
+	//2o .then() -> recibe la informacion en JS
+	.then((data)=>{
+		console.log(data);
+		setTodos(data.todos)
+	})
+	//si ocurre algun error aqui se maneja
+	.catch((err)=>{
+		console.log(err);
+		
+	})
+	}
+	function addTodo(){
+		fetch("https://playground.4geeks.com/todo/todos/cenicerolleno", {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+		.then((response) => { return response.json( )})
+		.then()
+		.catch()
+	}
 	return (
 		<div className="text-center">
-            
+            <p>holi</p>
+			{todos.map((value)=>{
+				return <h1>{value.label}</h1>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			})}
+			
 		</div>
 	);
 };
