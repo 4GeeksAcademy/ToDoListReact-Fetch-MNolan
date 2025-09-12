66178a16c3d4fae7cc8629f8503fc8eea82d35c9
Mauro Nolan <mrnolan88@gmail.com>
2025-09-12 19:37:45 +0000

    primera incorporacion de useEffect

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,35 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-//URL de la API: https://playground.4geeks.com/todo/docs#/User%20operations/read_user_users__user_name__get
+//URL del endpoint (API): https://playground.4geeks.com/todo/docs#/User%20operations/read_user_users__user_name__get
 
 //create your first component
 const Home = () => {
 	const [todos, setTodos] = useState([])
-	function getTodos(){
+	const [addTodos, setAddTodos] = useState("")
+	/*//useEfect recibe 2 parametros
+	//			1er-> funcion 
+	//			2o (opcional)-> demarca cual es de los primeros 3 casos
+	//					no hay 2 parametros -> ejecuta la funcion cada vez que
+	//											se rerenderiza el componente
+	//								[] -> ejecuta la funcion una sola vez
+	//								[var1, var2, ..] -> ejecuta la funcion cada vez
+	//									que cambia una de las variables listadas
+	//    **Sepueden declarar todos los useEffect() que quieras pero solo uno de ellos
+	//			podra declararse sin el array de dependencias				
+	//useEffect()*/
+	useEffect(()=>{
+		console.log("Funcion useEfefect llamada");
+		getTodos()
+		//return
+	},[])
 
-	
-	//fetch() -> url del endpoint, enviar el body
-	fetch("https://playground.4geeks.com/todo/users/cenicerolleno",{
-		method:"GET"
-	})
-	// status code , recibe la informacion en JSON, convertimos a JS y l aenviamos 
-	//al siguiente .then()
-	.then((response)=>{
-		console.log(response);
-		return response.json() // .json() convertir de json a JS y enviarlo al siguiente
-	})
-	//2o .then() -> recibe la informacion en JS
-	.then((data)=>{
-		console.log(data);
-		setTodos(data.todos)
-	})
-	//si ocurre algun error aqui se maneja
-	.catch((err)=>{
-		console.log(err);
-		
-	})
+	function getTodos() {
+
+
+		//fetch() -> url del endpoint, enviar el body
+		fetch("https://playground.4geeks.com/todo/users/cenicerolleno", {
+			//method: "GET" //Por defecto si solo se envia el fetch sin un metodo,
+		}) 				//  siempre se asignará el método GET 
+			// status code , recibe la informacion en JSON, convertimos a JS y l aenviamos 
+			//al siguiente .then()
+			.then((response) => {
+				console.log(response);
+				return response.json() // .json() convertir de json a JS y enviarlo al siguiente
+			})
+			//2o .then() -> recibe la informacion en JS
+			.then((data) => {
+				console.log(data);
+				setTodos(data.todos)
+			})
+			//si ocurre algun error aqui se maneja
+			.catch((err) => {
+				console.log(err);
+
+			})
 	}
-	function addTodos(){
+	function addTodo() {
+		setAddTodos(addTodos)
 		let data = {
 			label: "Ir al peluquero",
 			is_done: false
@@ -42,28 +61,28 @@ const Home = () => {
 			},
 			body: JSON.stringify(data)
 		})
-		//1er .then() status code, recibe la informacion en JSON, convertimos a JS
-		// y la enviamos al siguiente .then() 
-		.then((response) => { 
-			return response.json( ) // .json() convertir de JSON a JS y enviarlo al siguiente .then()
-		})
-		//2o .then() -> recibe la informacion en JS
-		.then((data)=>{
-			console.log(data);
-			
-		})
-		.catch((err)=>{ err })
+			//1er .then() status code, recibe la informacion en JSON, convertimos a JS
+			// y la enviamos al siguiente .then() 
+			.then((response) => {
+				return response.json() // .json() convertir de JSON a JS y enviarlo al siguiente .then()
+			})
+			//2o .then() -> recibe la informacion en JS
+			.then((data) => {
+				console.log(data);
+
+			})
+			.catch((err) => { err })
 	}
 	return (
 		<div className="text-center">
-            <h1>Holi</h1>
-			<button className="btn btn-success" onClick={()=>{
-				getTodos();
-			}}>Get Todos</button>
-			<button className="btn btn-primary" onClick={()=>{
-				addTodos();
+			<h1>Holi</h1>
+			
+				
+			
+			<button className="btn btn-primary" onClick={() => {
+				addTodo();
 			}}>Add Todos</button>
-			{todos.map((value)=>{
+			{todos.map((value) => {
 				return <h4>{value.label}</h4>
 
 			})}
